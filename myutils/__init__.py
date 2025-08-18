510febf0fb77f8349167c31947a38e6c8a4aa4f0
norths7ar <guicaitiandu@gmail.com>
2025-08-18 09:34:46 -0400

    Move clean_json_identifier from text.py to llm_config.py

--- myutils/__init__.py
+++ myutils/__init__.py
@@ -8,10 +8,10 @@ from .io import (load_json,
                  load_md,
                  set_output_dir)
 from .llm_config import (LLMConfig,
+                         clean_json_identifier,
                          create_llm_config,
                          get_llm_response)
 from .logger import get_logger, init_step_logger
-from .text import clean_json_identifier
 
 __all__ = [
     "parse_args", "parse_args_benchmark",
